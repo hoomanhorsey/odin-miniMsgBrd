bd9d686aa9de778f9a3d1f6d7f7e627280a8e1a0
Hermshy Mac <94696321+hoomanhorsey@users.noreply.github.com>
2025-12-09 15:08:00 +1100

    index router sync

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -7,9 +7,7 @@ router.get("/", controller.getMessages);
 router.get("/message/:index", controller.displayMessage);
 
 // Create controller file for /new, get and post
-
 router.get("/new", controller.renderNewForm);
-
 router.post("/new", controller.submitNewMessage);
 
 module.exports = router;
